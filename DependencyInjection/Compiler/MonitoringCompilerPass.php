d2b35cacd9bfc85c198cb0842af2609fdd3989dd
kzi <kevin.ziemianski@canaltp.fr>
2014-06-20 17:27:55 +0200

    Change xml output and add compilerPass

---
+++ DependencyInjection/Compiler/MonitoringCompilerPass.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CanalTP\SamMonitoringBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class MonitoringCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $definition = $container->getDefinition(
+            'sam_monitoring_manager'
+        );
+
+        $taggedServices = $container->findTaggedServiceIds(
+            'service_monitoring'
+        );
+        
+        foreach ($taggedServices as $id => $attributes) {
+            $matches = array();
+            $application = '';
+            if (preg_match('/(\w*)\./', $id, $matches)) {
+                $application = $matches[1];
+
+                foreach ($attributes as $key => $value) {
+                    if (isset($value['category'])) {
+                        $definition->addMethodCall(
+                            'addService',
+                            array(new Reference($id), $application, $value['category'])
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
